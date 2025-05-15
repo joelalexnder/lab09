2fed6d6ed8f10cbea6452faff6bd46cc4c31b293
joelalexnder <wksx9d@gmail.com>
2025-05-15 14:58:02 -0500

    add: PostApiService

---
+++ app/src/main/java/com/example/lab09/api/PostApiService.kt
@@ -0,0 +1,14 @@
+package com.example.lab09.api
+
+import com.example.lab09.model.PostModel
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface PostApiService {
+
+    @GET("posts")
+    suspend fun getUserPosts(): List<PostModel>
+
+    @GET("posts/{id")
+    suspend fun getUserPostById(@Path("id") id: Int): PostModel
+}
